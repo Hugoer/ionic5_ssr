dd7534f1fe8f2be18a665019acaf42fc97332f60
Hugoerdev <hugoerdev@gmail.com>
2020-06-30 20:01:06 +0200

    Improve express server

--- server.ts
+++ server.ts
@@ -1,19 +1,78 @@
+// tslint:disable: no-string-literal
 import 'zone.js/dist/zone-node';
 
 import { ngExpressEngine } from '@nguniversal/express-engine';
 import * as express from 'express';
 import { join } from 'path';
 
+// ssr DOM
+// const domino = require('domino');
+// const fs = require('fs');
+// const path = require('path');
+// // index from browser build!
+// const template = fs.readFileSync(path.join('.', 'dist', 'app', 'browser', 'index.html')).toString();
+// // for mock global window by domino
+// const win = domino.createWindow(template);
+// // (window as any).global = win;
+// global['window'] = win;
+// global['document'] = win.document;
+
 import { AppServerModule } from './src/main.server';
 import { APP_BASE_HREF } from '@angular/common';
 import { existsSync } from 'fs';
+import * as compression from 'compression';
+import * as cookieparser from 'cookie-parser';
+global['XMLHttpRequest'] = require('xhr2');
 
-// The Express app is exported so that it can be used by serverless Functions.
 export function app() {
   const server = express();
   const distFolder = join(process.cwd(), 'dist/app/browser');
   const indexHtml = existsSync(join(distFolder, 'index.original.html')) ? 'index.original.html' : 'index';
+  // redirects!
+  const redirectowww = false;
+  const redirectohttps = false;
+  const wwwredirecto = true;
+  server.use((req, res, next) => {
+    // for domain/index.html
+    if (req.url === '/index.html') {
+      res.redirect(301, 'https://' + req.hostname);
+    }
 
+    // check if it is a secure (https) request
+    // if not redirect to the equivalent https url
+    if (
+      redirectohttps &&
+      req.headers['x-forwarded-proto'] !== 'https' &&
+      req.hostname !== 'localhost'
+    ) {
+      // special for robots.txt
+      if (req.url === '/robots.txt') {
+        next();
+        return;
+      }
+      res.redirect(301, 'https://' + req.hostname + req.url);
+    }
+
+    // www or not
+    if (redirectowww && !req.hostname.startsWith('www.')) {
+      res.redirect(301, 'https://www.' + req.hostname + req.url);
+    }
+
+    // www or not
+    if (wwwredirecto && req.hostname.startsWith('www.')) {
+      const host = req.hostname.slice(4, req.hostname.length);
+      res.redirect(301, 'https://' + host + req.url);
+    }
+
+    // for test
+    // if (test && req.url === '/test/exit') {
+    //   res.send('exit');
+    //   exit(0);
+    //   return;
+    // }
+
+    next();
+  });
   // Our Universal express-engine (found @ https://github.com/angular/universal/tree/master/modules/express-engine)
   server.engine('html', ngExpressEngine({
     bootstrap: AppServerModule,
@@ -42,6 +101,10 @@ function run(): void {
 
   // Start up the Node server
   const server = app();
+  // gzip
+  server.use(compression());
+  // cokies
+  server.use(cookieparser());
   server.listen(port, () => {
     console.log(`Node Express server listening on http://localhost:${port}`);
   });
